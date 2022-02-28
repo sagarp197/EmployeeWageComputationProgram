59c7521ecb91de4c9add3e9f18f1f6f77897daae
sagarp197 <99491106+sagarp197@users.noreply.github.com>
2022-02-28 19:52:43 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,12 +1,30 @@
-﻿namespace EmployeeWageProgram
+namespace EmployeeWageProgram
 {
-
-    class Program
+    public class EmpWage
     {
-        static void Main(string[] args)
+        public static void CalculateEmpWage()
         {
-            Console.WriteLine("Welcome to Employee Wage Program");
-        }
+            int Is_Full_Time = 0;
+            int Emp_Rate_Per_Hour = 20;
+            int empHrs = 0;
+            int empWage = 0;
 
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+            }
+            empWage = empHrs * Emp_Rate_Per_Hour;
+            Console.WriteLine("Emp Wage :" + empWage);
+
+        }
     }
 }
