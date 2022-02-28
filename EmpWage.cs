f5596aa2534e9ddc3eb6e0a524f113a393e9eabd
sagarp197 <sagarp197@gmail.com.com>
2022-02-28 18:35:24 +0530

    Employee Wage

---
+++ EmpWage.cs
@@ -0,0 +1,30 @@
+﻿namespace EmployeeWageProgram
+{
+    public class EmpWage
+    {
+        public static void CalculateEmpWage()
+        {
+            int Is_Full_Time = 0;
+            int Emp_Rate_Per_Hour = 20;
+            int empHrs = 0;
+            int empWage = 0;
+
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if (empCheck == Is_Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+                empHrs = 8;
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                empHrs = 0;
+            }
+            empWage = empHrs * Emp_Rate_Per_Hour;
+            Console.WriteLine("Emp Wage :" + empWage);
+
+        }
+    }
+}
