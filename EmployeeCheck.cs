21a355fda0d351e94722b2dfd17f218ae2ef614d
sagarp197 <sagarp197@gmail.com.com>
2022-02-28 14:56:40 +0530

    Employee Attendance

---
+++ EmployeeCheck.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageComputationProgram
+{
+    public class EmployeeCheck
+    {
+        public static void CheckEmployeePresence()
+        {
+            int Is_Full_Time = 1;
+
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            if(empCheck==Is_Full_Time)
+            {
+                Console.WriteLine("Employee is Present");
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+            }
+        }
+    }
+}
