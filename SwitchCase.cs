9d188cdb7480949782e5ab614f0c1e5d7e1c48f4
sagarp197 <sagarp197@gmail.com.com>
2022-03-01 20:21:41 +0530

    Switch Case Exmpl

---
+++ SwitchCase.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeWageProgram
+{
+    public class SwitchCase
+    {
+        public static void CalculateEmpWage()
+        {
+            int Emp_Rate_Per_Hour = 20;
+            int empHrs = 0;
+            int empWage = 0;
+            Random random = new Random();
+            int empCheck = random.Next(2);
+
+            //Console.WriteLine("Enter an alphabet");
+            //ch = Convert.ToChar(Console.ReadLine());
+
+            switch (empCheck)
+            {
+                case 0:
+                    Console.WriteLine("Employee is Present");
+                    empHrs = 8;
+                    empWage = empHrs * Emp_Rate_Per_Hour;
+                    Console.WriteLine("Emp Wage :" + empWage);
+                    break;
+                
+                case 1:
+                    Console.WriteLine("Employee is Working Part Time");
+                    empHrs = 4;
+                    empWage = empHrs * Emp_Rate_Per_Hour;
+                    Console.WriteLine("Emp Wage :" + empWage);
+                    break;
+
+                default:
+                    Console.WriteLine("Employee is Absent");
+                    empHrs = 0;
+                    empWage = empHrs * Emp_Rate_Per_Hour;
+                    Console.WriteLine("Emp Wage :" + empWage);
+                    break;
+            }
+        }
+    }
+}
